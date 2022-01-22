0768f659f478df238d4489e73fb8ba95c14eeec3
ddijk <dick@dijkrosoft.nl>
2022-01-22 10:56:29 +0100

    update endpoints and payload format

--- index.js
+++ index.js
@@ -19,38 +19,35 @@ app.options('/case/overzicht-bgk', (req, res) => {
   res.sendStatus(200);
 })
 
+app.options('/case/overzicht-bgk/count', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+})
+
+
+
+
+
+app.post('/case/overzicht-bgk/count', (req, res) => {
+
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.status(200).send({"folders":[1313],"totalSize":tableData.content.length})
+
+})
 
 app.post('/case/overzicht-bgk', (req, res) => {
+  const i = req.body.chunkIndex;
+  console.log(`index is ${i}`)
+  const n = 2;
 
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.status(200).send(tableData)
-
-})
-
-app.get('/get_stuff', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-
-  res.status(200).send(tableData)
-
-})
-
-app.get('/get_total_size', (req, res) => {
-
-
-  const total = { "total": tableData.length }
-  res.status(200).send(total)
-})
-
-app.get('/get_chunk', (req, res) => {
-  const n = Number(req.query['n'])
-  const i = Number(req.query['i'])
-  console.log('chunk size', n)
-  console.log('chunk index', i)
   res.setHeader('Access-Control-Allow-Origin', '*')
 
   setTimeout((() => {
-    res.status(200).send({ "total_size": tableData.length, "chunk_index": i, "chunk_size": n, "content": tableData.slice(n * i, n * (i + 1)) })
-  }), 2000)
+    res.status(200).send({ "total_size": tableData.content.length, "chunkIndex": i, "chunkSize": n, "content": tableData.content.slice(n * i, n * (i + 1)) })
+  }), 100)
 
 })
 
